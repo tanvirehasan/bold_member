af147c0696d1eaba5c0f027ac8f01fb45f916a4e
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2022-06-02 19:18:34 +0600

    new upload
    new upload

---
+++ membership.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>BOLD - New Membership</title>
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
+  <link rel="stylesheet" href="assets/vendors/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="shortcut icon" href="uploads/brand/logo.png" />
+</head>
+<body>
+
+<div class="container my-5">
+
+<div class="bg-white pt-0">
+
+<div class="from_header text-center bg-light p-5">
+    <img src="uploads/brand/logo.png" alt="" >
+    <h1 class="p-2 h2 mt-2 text-drack text-uppercase" >MEMBERSHIP APPLICATION</h1>
+</div>
+
+    
+        <form class="row g-3 p-5">
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Name</label>
+                <input type="email" class="form-control" id="inputEmail4">
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Phone Number</label>
+                <input type="password" class="form-control" id="inputPassword4">
+            </div>
+
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Email</label>
+                <input type="email" class="form-control" id="inputEmail4">
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Password</label>
+                <input type="password" class="form-control" id="inputPassword4">
+            </div>
+
+            <div class="col-md-6">
+                <label for="Dateofbirth" class="form-label">Date of birth</label>
+                <input type="date" class="form-control" id="Dateofbirth">
+            </div>
+            <div class="col-md-6">
+                <label for="profile" class="form-label">Profile Link</label>
+                <input type="text" class="form-control" id="profile" placeholder="Facebook/Linkedin/Twitter..">
+            </div>
+
+            <div class="col-12">
+                <label for="inputAddress" class="form-label">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+
+            <div class="col-md-6">
+                <label for="inputCity" class="form-label">City</label>
+                <input type="text" class="form-control" id="inputCity">
+            </div>
+            <div class="col-md-4">
+                <label for="inputState" class="form-label">State</label>
+                <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">Zip</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+
+            <label for="" class="px-2 p-0 m-0 mt-5 mb-3 fw-bold ">Job/Career* <hr class=" p-0 m-0"></label>
+
+
+
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Experiences</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Your Total Job/Career Experiences Years & Months Ex:(5 Y, 10 M)">
+            </div>
+
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Organization</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Current Organization">
+            </div>
+
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Position/Designation</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Your Current Position/Designation">
+            </div>
+
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Reference</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Reference (if any; write his or her name):">
+            </div>
+
+            <div class="col-md-12">
+                <label for="inputEmail4" class="form-label">About Your Self</label>
+                <textarea class="form-control" name="" id="" cols="30" rows="10">Your profile ( please write a paragraph about yourself not exceeding 100 words):
+                </textarea>
+            </div>
+
+            <div class="col-md-4">
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Your Photo</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Your CV</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Your signature(if available)</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+            </div>
+
+
+
+
+            <hr class="p-0 mt-5">
+            <div class="col-6 px-3">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Accept the <a href=""> Terms</a> & <a href=""> Conditions</a>. 
+                </label>
+                </div>
+            </div>
+
+
+            <div class="col-6 text-end  px-3">               
+                <button type="submit" class="btn btn-success p-2 px-5">Submit</button>
+            </div>
+
+        </form>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- plugins:js -->
+  <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="assets/vendors/chartjs/Chart.min.js"></script>
+  <script src="assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+  <script src="assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="assets/js/material.js"></script>
+  <script src="assets/js/misc.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="assets/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+</body>
+</html> 
